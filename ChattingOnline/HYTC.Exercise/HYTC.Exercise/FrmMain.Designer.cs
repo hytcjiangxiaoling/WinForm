c13885622aeb14862091cfc3d920feb55d2801e7
js8jxl07 <296528724@qq.com>
2014-08-25 16:53:12 +0800

    2014-08-25
    1.WinForm...ChatOnline

---
+++ ChattingOnline/HYTC.Exercise/HYTC.Exercise/FrmMain.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace HYTC.Exercise
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtHistory = new System.Windows.Forms.RichTextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(12, 12);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(433, 21);
+            this.txtIP.TabIndex = 0;
+            this.txtIP.Text = "255.255.255.255";
+            // 
+            // txtHistory
+            // 
+            this.txtHistory.Location = new System.Drawing.Point(12, 49);
+            this.txtHistory.Name = "txtHistory";
+            this.txtHistory.ReadOnly = true;
+            this.txtHistory.Size = new System.Drawing.Size(433, 144);
+            this.txtHistory.TabIndex = 1;
+            this.txtHistory.Text = "";
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(12, 208);
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(433, 21);
+            this.txtMsg.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(370, 249);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 284);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.txtIP);
+            this.Name = "FrmMain";
+            this.Text = "聊天室";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.RichTextBox txtHistory;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ChattingOnline/HYTC.Exercise/HYTC.Exercise/FrmMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace HYTC.Exercise
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string ip = this.txtIP.Text;
+            UdpClient uc = new UdpClient();
+            string msg = "PUBLIC|"+this.txtMsg.Text+"|Mischief";
+            Byte[] bmsg = Encoding.Default.GetBytes(msg);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            uc.Send(bmsg, bmsg.Length, ipep);//字符串用length，数组用count
+        }
+
+        private void listen()
+        {
+            UdpClient uc = new UdpClient(9527);
+            while (true)
+            {
+                IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 0);
+                byte[] bmsg = uc.Receive(ref ipep);
+                string msg = Encoding.Default.GetString(bmsg);
+                string[] sp = msg.Split('|'); 
+                if (sp[0] == "INROOM")
+                {
+                    this.txtHistory.Text += sp[1] +"上线了！"+ "\r\n";
+                }
+                if (sp[0] == "PUBLIC")
+                {
+                    this.txtHistory.Text += sp[2] + "说：" + sp[1] + "\r\n";
+                }
+                if (sp[0] == "OUTROOM")
+                {
+                    this.txtHistory.Text += sp[1] + "离线啦"  + "\r\n";
+                }
+               
+            }
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            string ip = this.txtIP.Text;
+            UdpClient uc = new UdpClient();
+            string msg = "INROOM|"+"Mischief";
+            Byte[] bmsg = Encoding.Default.GetBytes(msg);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            uc.Send(bmsg, bmsg.Length, ipep);//字符串用length，数组用count
+
+            FrmMain.CheckForIllegalCrossThreadCalls = false;
+            Thread th = new Thread(new ThreadStart(listen));
+            th.IsBackground = true;
+            th.Start();
+        }
+
+        private void FrmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string ip = this.txtIP.Text;
+            UdpClient uc = new UdpClient();
+            string msg = "OUTROOM|" + "Mischief";
+            Byte[] bmsg = Encoding.Default.GetBytes(msg);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse(ip), 9527);
+            uc.Send(bmsg, bmsg.Length, ipep);
+
+        }
+    }
+}
